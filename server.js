a71631115d52a03552b279e147538723d7673f89
Giginio <gigi.margarito1@gmail.com>
2024-05-21 16:32:39 +0200

    changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps FS2024 :-))) This is a change in code');
+  res.send('Hello DevOps FS2024 :-))) This is the second change');
 });
 
 app.listen(PORT, HOST);
